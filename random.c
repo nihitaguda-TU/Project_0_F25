dfc91a6414e9578b0e13ed5c678793658c92debf
Nihita Guda <tur99077@temple.edu>
2025-08-31 19:43:55 -0400

    created skeleton of program

--- random.c
+++ random.c
@@ -4,4 +4,12 @@ of random characters (a word). The length of the word is a
 parameter in your program.
 '''
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void rand_string (char*s , size_t size)
+{
+
+}
 
