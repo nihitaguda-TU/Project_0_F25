fee5e37752a945b990d837b522d2391d8ee2fe36
Nihita Guda <tur99077@temple.edu>
2025-08-31 20:30:56 -0400

    removed triple quotes

--- random.c
+++ random.c
@@ -1,8 +1,3 @@
-''' The
-function rand_string() is a character function that returns a string
-of random characters (a word). The length of the word is a
-parameter in your program.
-'''
 
 #include <stdio.h>
 #include <stdlib.h>
