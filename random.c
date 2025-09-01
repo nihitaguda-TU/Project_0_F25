efef2ceacf1acde6b39700a346dd2fcb935046e3
Nihita Guda <tur99077@temple.edu>
2025-08-31 20:27:20 -0400

    final debugging, added another import and fixed for loop syntax

--- random.c
+++ random.c
@@ -7,13 +7,15 @@ parameter in your program.
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stddef.h>
+
 
 void rand_string (char *s, size_t size)
 {
-    for (size_t i = 0, i < size, i++){
+    for (size_t i = 0; i < size; i++){
         int rando = rand() % 26;
         s[i] = 'A' + rando;
     }
-    return s;
+    s[size] = '\0';               
 }
 
